f62e6957cffffb3362ed4b5fd9662a0fc08ab34a
h4sski.programming <h4sski.programming@gmail.com>
2024-11-11 23:27:43 +0100

    After small refactor.

--- main.py
+++ main.py
@@ -51,47 +51,25 @@ def main():
     
     driver.quit()
 
+
 def get_exhibitor(driver: webdriver, url: str) -> dict:
     driver.get(url)
     time.sleep(2)
     bs_exhibitor = BeautifulSoup(driver.page_source, 'html.parser')
     return {
-        'name': get_name(bs_exhibitor),
-        'description': get_description(bs_exhibitor),
-        'hall': get_hall(bs_exhibitor),
-        'address': get_address(bs_exhibitor),
-        'number': get_number(bs_exhibitor),
-        'email': get_email(bs_exhibitor),
-        'web': get_web(bs_exhibitor),
+        'name': get_value(bs_exhibitor, 'div', 'popup-name ng-binding'),
+        'description': get_value(bs_exhibitor, 'div', 'popup-desc ng-binding ng-scope'),
+        'hall': get_value(bs_exhibitor, 'span', 'popup-stand-text ng-binding'),
+        'address': get_value(bs_exhibitor, 'div', 'popup-street ng-binding'),
+        'number': get_value(bs_exhibitor, 'div', 'popup-phone ng-binding'),
+        'email': get_value(bs_exhibitor, 'div', 'popup-email'),
+        'web': get_value(bs_exhibitor, 'a', 'underline-animation ng-binding'),
     }
 
-def get_name(bs: BeautifulSoup) -> str:
-    div = bs.find('div', 'popup-name ng-binding')
-    return div.text.strip()
+def get_value(bs: BeautifulSoup, tag: str, class_: str) -> str:
+    tag = bs.find(tag, class_)
+    return tag.text.strip()
 
-def get_description(bs: BeautifulSoup) -> str:
-    div = bs.find('div', 'popup-desc ng-binding ng-scope')
-    return div.text.strip()
-
-def get_hall(bs: BeautifulSoup) -> str:
-    div = bs.find('span', 'popup-stand-text ng-binding')
-    return div.text.strip()
-
-def get_address(bs: BeautifulSoup) -> str:
-    div = bs.find('div', 'popup-street ng-binding')
-    return div.text.strip()
-
-def get_number(bs: BeautifulSoup) -> str:
-    div = bs.find('div', 'popup-phone ng-binding')
-    return div.text.strip()
-
-def get_email(bs: BeautifulSoup) -> str:
-    div = bs.find('div', 'popup-email')
-    return div.text.strip()
-
-def get_web(bs: BeautifulSoup) -> str:
-    div = bs.find('a', 'underline-animation ng-binding')
-    return div.get('href')
 
 if __name__ == '__main__':
     main()
