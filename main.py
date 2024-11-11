7867814ac5c8e67ce61c5a87ef66d8e90626e994
h4sski.programming <h4sski.programming@gmail.com>
2024-11-11 22:15:25 +0100

    Fix issue with selenium, can load the pages

--- main.py
+++ main.py
@@ -2,26 +2,29 @@ import requests
 import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
 
 def main():
-    driver = webdriver.Chrome()
-    # driver.implicitly_wait(2)
-    # base_url = 'https://www.amcham.org.eg/membership/members-database?keyword=&keywordtype=company&sector=0&subsector=0&membershiptype=0'
-    base_url = 'https://www.selenium.dev/selenium/web/web-form.html'
+    service = Service("/snap/bin/geckodriver")
+    driver = webdriver.Firefox(service=service)
+    # driver.implicitly_wait(5)
+    base_url = 'https://www.amcham.org.eg/membership/members-database?keyword=&keywordtype=company&sector=0&subsector=0&membershiptype=0'
     
     driver.get(base_url)
-    # time.sleep(10)
+    time.sleep(7)
     
-    # page_source = driver.page_source
-    print(driver.test)
+    title = driver.title
+    print(title)
     
-    # r = requests.get(base_url, verify=False)
+    ######################################
     
-    # bs = BeautifulSoup(page_source, 'html.parser')
+    page_source = driver.page_source
     
-    # item = bs.div(class_='thumbnail thumbnail-news')
-    # data = item
-    # print(data)
+    bs = BeautifulSoup(page_source, 'html.parser')
+    
+    item = bs.div(class_='thumbnail thumbnail-news')
+    data = item
+    print(data)
     
     # p = bs.find_all('div', 'exhibitors-main-container')
     # for pp in p:
